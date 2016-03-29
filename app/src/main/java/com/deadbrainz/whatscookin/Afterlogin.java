153403b0413e0ddf3709026e68ff1316b7633572
Birathepan Tharmakulasingam <birathet@stud.ntnu.no>
2016-03-29 13:49:59 +0200

    first push

---
+++ app/src/main/java/com/deadbrainz/whatscookin/Afterlogin.java
@@ -0,0 +1,24 @@
+package com.deadbrainz.whatscookin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Afterlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afterlogin);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        }
+    };
+
+
--- app/src/main/java/com/deadbrainz/whatscookin/MainActivity.java
+++ app/src/main/java/com/deadbrainz/whatscookin/MainActivity.java
@@ -1,9 +1,15 @@
 package com.deadbrainz.whatscookin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,4 +40,31 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private EditText username,password;
+
+
+
+
+    public void ButtononClick(View v){
+        username= (EditText) findViewById(R.id.brukernavn);
+        password= (EditText) findViewById(R.id.passord);
+
+
+        if(username.getText().toString().equals("brukernavn") && password.getText().toString().equals("passord")){
+            startActivity(new Intent(MainActivity.this, Afterlogin.class));
+
+        }else {
+            Toast toast= new Toast(getApplicationContext());
+            Toast.makeText(getApplicationContext(), "Wrong inputs " + username.getText() +" "+password.getText(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+
+
+
+
 }
